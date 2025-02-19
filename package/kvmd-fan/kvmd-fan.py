77bee570510b4d0a631fd342f60a9dd7687f4ab3
Gustavo Sampaio <gbritosampaio@gmail.com>
2025-02-19 20:39:33 -0300

    unix sock fan monitor

--- package/kvmd-fan/kvmd-fan.py
+++ package/kvmd-fan/kvmd-fan.py
@@ -3,6 +3,9 @@
 
 import RPi.GPIO
 import time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn, UnixStreamServer
+import os
 
 #settings
 fan_gpio=12 # Fan GPIO pin number (default: 12)
@@ -10,12 +13,60 @@ idle_speed = 0 # Fan speed when under min_temp(IDLE) (min: 0, max: 100)
 min_temp = 60 # Fan starting temperature 
 max_temp = 65 # Fan max speed temperature (max: 65)
 
+VERSION = "0.1"
+SOCKET_PATH = "/run/kvmd/fan.sock"
+
+# Remove existing socket file if it exists
+if os.path.exists(SOCKET_PATH):
+    os.remove(SOCKET_PATH)
+
 #define GPIO
 RPi.GPIO.setwarnings(False)
 RPi.GPIO.setmode(RPi.GPIO.BCM)
 RPi.GPIO.setup(fan_gpio, RPi.GPIO.OUT)
 pwm = RPi.GPIO.PWM(fan_gpio,100)
 running = False
+cpu_temp = 0.0
+fan_speed = 0.0
+
+def running_status():
+	if running:
+		return "true"
+	return "false"
+
+class Handler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		content = "Stub"
+		content_type = "text/plain"
+		status = 200
+
+		if self.path == '/':
+			content_type = "application/json"
+			content = f'{"ok": true, "result": {"version": "{VERSION}"}}\n'
+		elif self.path == '/state':
+			content_type = "application/json"
+			content = ( '{"ok": true, "result": {'
+										 f'"service": {"now_ts": 0.01},'
+										f' "temp": {"real": {cpu_temp:.2f}, "fixed": {cpu_temp:.2f}},'
+										f' "fan": {"speed": {fan_speed:.2f}, "pwm": 0, "ok": {running_status()}, "last_fail_ts": 0.01},'
+										 ' "hall": {"available": false, "rpm": 0}'
+										 f'}}\n' )
+		else:
+			status = 404
+			content = "Not Found\n"
+
+		self.send_response_only(status)
+		self.send_header('Content-type', content_type)
+		self.end_headers()
+		self.wfile.write(bytes(content, "utf-8"))
+
+class UnixSocketHttpServer(ThreadingMixIn, UnixStreamServer):
+    def get_request(self):
+        request, client_address = super(UnixSocketHttpServer, self).get_request()
+        return (request, ["local", 0])
+
+server = UnixSocketHttpServer(SOCKET_PATH, Handler)
+server.serve_forever()
 
 #running code
 try:
@@ -35,6 +86,7 @@ try:
 				temp_speed = int((cpu_temp - min_temp) / (max_temp - min_temp) * 100) # set 0~100% for min and max temp ranges
 				set_speed = min(max(idle_speed, temp_speed), 100) # make speed not over 100 
 			print("set_speed:",set_speed,"%") 
+			fan_speed = set_speed
 			pwm.ChangeDutyCycle(set_speed)
 		else :
 			if(running):
@@ -47,3 +99,5 @@ try:
 except KeyboardInterrupt:
 		pass
 pwm.stop()
+server.server_close()
+os.remove(SOCKET_PATH)
